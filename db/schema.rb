04192a596d5ab2d62e0983d4ddae8a7c589e46a8
Luis089 <sirluis50@googlemail.com>
2020-06-24 15:26:13 +0200

    migration deleted

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_06_24_115538) do
-
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
 end
