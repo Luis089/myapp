59b79c2235fafae8e3973785f969c2428baa03bb
Luis089 <sirluis50@googlemail.com>
2020-06-24 15:08:41 +0200

    asd

--- db/migrate/20200624125214_create_articles.rb
+++ db/migrate/20200624125214_create_articles.rb
@@ -1,7 +1,13 @@
 class CreateArticles < ActiveRecord::Migration[6.0]
-  def change
-    create_table :articles do |t|
+def up
+    if table_exists?(:articles)
+      # Some changed if required
+    else
+     create_table :articles do |t|
       t.string :title
     end
+  end
+
+  def down   
   end
 end
