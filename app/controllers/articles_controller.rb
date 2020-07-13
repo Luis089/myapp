525abf8fdfe424b7724e1a965f86d7b790ea2bf1
Luis089 <sirluis50@googlemail.com>
2020-07-13 16:55:41 +0200

    asdasd

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,5 +11,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
   end
 end
