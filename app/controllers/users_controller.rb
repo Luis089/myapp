06521c9212598ef9640f2e37b3adcf1643934b9d
Luis089 <sirluis50@googlemail.com>
2020-07-30 16:51:15 +0200

    finished sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :require_user, only: [:edit, :update]
   before_action :require_same_user, only: [:edit, :update]
 
@@ -37,6 +37,10 @@ class UsersController < ApplicationController
     else
       render "new"
     end
+  end
+
+  def destroy
+    @user.destroy
   end
 
   private
