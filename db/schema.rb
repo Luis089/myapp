95d2bbb4f90287e45c3fed7f111fff4204a6e7d8
Luis089 <sirluis50@googlemail.com>
2020-08-10 01:45:27 +0200

    Categories Backend and UI

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_07_115356) do
+ActiveRecord::Schema.define(version: 2020_08_09_223247) do
+
+  create_table "article_categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title"
